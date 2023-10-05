6450bdffb6a37c754d35f3810732eff75813143f
Bill Marban <billym41620@gmail.com>
2023-10-05 11:50:41 -0700

    Finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/TPP_LOGO.PNG";
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#E6E6FA">
-    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
+    <Stack
+      gap="40px"
+      sx={{ alignItems: "center" }}
+      flexWrap="wrap"
+      px="40px"
+      pt="24px"
+    >
+      <img src={Logo} alt="logo" style={{ width: "200px", height: "41px" }} />
     </Stack>
     {/* <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by JavaScript Mastery</Typography> */}
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Lets do Karaoke! <br/> -The CEI Coders </Typography>
+    <Typography
+      variant="h5"
+      sx={{ fontSize: { lg: "28px", xs: "20px" } }}
+      mt="41px"
+      textAlign="center"
+      pb="40px"
+    >
+      Lets do Karaoke! <br /> -The CEI Coders{" "}
+    </Typography>
   </Box>
 );
 
